bda0e40f43b8f0d5a1dcc6017269ead5cd78b07d
Atul <atulreso1@gmail.com>
2024-06-24 17:17:35 +0530

    add: pwa-services

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import Layout from "./Layout";
 import { routerAll } from "./router";
+import { registerSW } from "virtual:pwa-register";
 
 function App() {
+  useEffect(() => {
+    const updateSW = registerSW({
+      onNeedRefresh() {
+        // Show a prompt to the user to reload the page
+        if (confirm("New content is available. Would you like to refresh?")) {
+          updateSW(true);
+        }
+      },
+      onOfflineReady() {
+        console.log("App is ready to work offline");
+      },
+    });
+  }, []);
+
   return (
     <>
       <Provider store={store}>
